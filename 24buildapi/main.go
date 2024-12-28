01d72ee53477e72a3a05e2f490fe4729fc61a326
abdullahalsazib <mdsazibhossin2021@gmail.com>
2024-12-28 23:52:13 +0600

    28-Dec-24|11:52PM

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Course struct {
 	CourseId    string  `json:"courseid"`
@@ -25,4 +29,17 @@ func IsEmpty(c *Course) bool {
 func main() {
 	fmt.Println("welcome in build api")
 
+}
+
+// controller file
+
+// serve home page
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to the Go home page or Api by Jack Sparrow</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all Corses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder().Encode(courseDB, )
 }
