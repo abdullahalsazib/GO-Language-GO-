b26b4120ed42d7d5443e8e681979240c9500371f
abdullahalsazib <mdsazibhossin2021@gmail.com>
2024-12-30 14:59:39 +0600

    30-Dec-24|2:59PM

--- myTaskAndProject/17CRUD/main.go
+++ myTaskAndProject/17CRUD/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
+
+var myUrl = "https://jsonplaceholder.typicode.com/todos"
 
 type Todo struct {
 	UserID    int    `json:"userId"`
@@ -15,6 +19,18 @@ type Todo struct {
 
 func main() {
 	fmt.Println("Learning CRUD....")
+	PostRequest()
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetMethod() {
+	fmt.Println("GET - Method")
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	checkError(err)
 
@@ -32,11 +48,36 @@ func main() {
 	checkError(err)
 
 	fmt.Println(todo.UserID)
-
 }
 
-func checkError(err error) {
-	if err != nil {
-		panic(err)
+func PostRequest() {
+	fmt.Println("POST - Method")
+	todo := Todo{
+		UserID:    213,
+		Title:     "just lorem text",
+		Completed: false,
 	}
+
+	// convert into json
+
+	jsonData, err := json.Marshal(todo)
+	checkError(err)
+
+	// convert json data to string !
+
+	jsonString := string(jsonData)
+
+	// convert string to json reader
+
+	jsonReader := strings.NewReader(jsonString)
+
+	// http post method
+	res, err := http.Post(myUrl, "aplication/json", jsonReader)
+	checkError(err)
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	checkError(err)
+	fmt.Println(string(data))
+
 }
