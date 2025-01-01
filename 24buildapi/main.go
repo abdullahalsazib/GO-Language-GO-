74ba05dce2b3802d3fb66fd52a81fd54bee94937
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-01 22:58:10 +0600

    1_Jan_25|10:58PM

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -19,173 +18,184 @@ type Course struct {
 	Author      *Author `json:"author"`
 }
 
+// Create a Author
 type Author struct {
-	Fullname string `json:"fullname"`
+	FullName string `json:"fullname"`
 	Website  string `json:"website"`
 }
 
-// fake DB - Database
 var courseDB []Course
 
-// Middleware, helper - file
+// Middleware, hepler - file
 func IsEmpty(c *Course) bool {
-	// return c.CourseId == "" && c.CourseName == ""
 	return c.CourseName == ""
 }
 
 func main() {
-	fmt.Println("welcome in build api")
-
+	fmt.Println("Hello, World!")
 	r := mux.NewRouter()
 
-	// seeding
+	// Seeding
 	courseDB = append(courseDB, Course{
 		CourseId:    "1",
 		CourseName:  "Go",
 		CoursePrice: 100,
 		Author: &Author{
-			Fullname: "Jack Sparrow",
-			Website:  "https://learncodeonline.in",
+			FullName: "Jack",
+			Website:  "https://udemy.com",
 		},
 	})
-
 	courseDB = append(courseDB, Course{
 		CourseId:    "2",
-		CourseName:  "Node",
-		CoursePrice: 200,
+		CourseName:  "Java",
+		CoursePrice: 160,
 		Author: &Author{
-			Fullname: "John Doe",
-			Website:  "https://learncodeonline.in",
+			FullName: "Sparrow",
+			Website:  "https://udemy.com",
 		},
 	})
 
-	courseDB = append(courseDB, Course{
-		CourseId:    "3",
-		CourseName:  "React",
-		CoursePrice: 300,
-		Author: &Author{
-			Fullname: "Sparrow Doe",
-			Website:  "https://learncodeonline.in",
-		},
-	})
-
-	// routhing
-	r.HandleFunc("/", serveHome).Methods("GET")
+	// routing -->
+	r.HandleFunc("/", homeServer).Methods("GET")
 	r.HandleFunc("/courses", getAllCourses).Methods("GET")
 	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
-	r.HandleFunc("/course", createOnecourse).Methods("POST")
-	r.HandleFunc("/course/{id}", updateOnecourse).Methods("PUT")
-	r.HandleFunc("/course/{id}", deleteOnecourse).Methods("DELETE")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
 
-	// listen on port 8080
-	log.Fatal(http.ListenAndServe(":8080", r))
+	// Define a custom 404 handler
+	r.NotFoundHandler = http.HandlerFunc(curstom404Handler)
 
+	// Surve a localhost server http://localhost:8080
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", r)
 }
 
-// controller file
+func homeServer(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>HOME Server, Hello, World!</h1>"))
+}
+func curstom404Handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`<div style="
+	width: 100%;
+    height: 100vh;
+    background: gray;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+	text-align: center;
+	">
+	<h1>404 PAGE !</h1>
+	<button onclick="alert('hello')">click me</button>
+	</div>`))
+}
 
-// serve home page
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Welcome to the Go home page or Api by Jack Sparrow</h1>"))
+// Check Error
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func getAllCourses(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get all Corses")
+	fmt.Println("Get the all Courses!")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courseDB)
 }
 
 func getOneCourse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get One Corses")
+	fmt.Println("Get one course")
 	w.Header().Set("Content-Type", "application/json")
 
 	// grap id from request
-	params := mux.Vars(r)
+	parems := mux.Vars(r)
 
-	// loop through course, fiind mathicing id and return the response
+	// loop thorugh course, find mathiching id and return the response
 	for _, course := range courseDB {
-		if course.CourseId == params["id"] {
+		if course.CourseId == parems["id"] {
 			json.NewEncoder(w).Encode(course)
 			return
 		}
+
 	}
 	json.NewEncoder(w).Encode("No course found with this id")
-	// return
 }
 
-func createOnecourse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create One Corses")
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create One Course")
 	w.Header().Set("Content-Type", "application/json")
 
-	// what if: body is empty
+	// what if : body is empty
 	if r.Body == nil {
-		json.NewEncoder(w).Encode("Please send some data")
+		json.NewEncoder(w).Encode("Please send some data..!")
 	}
 
-	// what about - {}
+	// What about - {}
 	var course Course
-	_ = json.NewDecoder(r.Body).Decode(&course)
+	err := json.NewDecoder(r.Body).Decode(&course)
+	checkError(err)
 
 	if IsEmpty(&course) {
-		json.NewEncoder(w).Encode("No data inside JSON")
-		return
+		json.NewEncoder(w).Encode("No Data Inside JSON..!")
 	}
+
 	for _, c := range courseDB {
 		if c.CourseName == course.CourseName {
-			json.NewEncoder(w).Encode("Course already exists")
-			return
+			json.NewEncoder(w).Encode("Course alrady exists..!")
 		}
 	}
 
-	// generate unique id, string
-	// append to courseDB
+	// // append to courseDB
 
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixMicro())
 	course.CourseId = strconv.Itoa(rand.Intn(100))
 	courseDB = append(courseDB, course)
 	json.NewEncoder(w).Encode(course)
-	// return
+
 }
 
-func updateOnecourse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Update One Corses")
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update one course")
 	w.Header().Set("Content-Type", "application/json")
 
-	// first - grap id from requesr
+	// first - grap id form request
 	params := mux.Vars(r)
 
-	// loop, id, remove, add with my ID
+	// loop, id , remove, add with my ID
 	for index, course := range courseDB {
 		if course.CourseId == params["id"] {
 			courseDB = append(courseDB[:index], courseDB[:index+1]...)
 			var course Course
-			_ = json.NewDecoder(r.Body).Decode(&course)
+			err := json.NewDecoder(r.Body).Decode(&course)
+			checkError(err)
 			course.CourseId = params["id"]
 			courseDB = append(courseDB, course)
 			json.NewEncoder(w).Encode(course)
 			return
 		}
 	}
-	// TODO: sned a response when id is not found
+
+	// TODO: send a response when id is not found
 	for _, course := range courseDB {
 		if course.CourseId == params["id"] {
-			json.NewEncoder(w).Encode("No course found with this id")
+			json.NewEncoder(w).Encode("No Course found with this id")
 		}
 	}
-
 }
 
-func deleteOnecourse(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Delete One Corses")
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete One Course")
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
-	// loop, id, remove (index, index+1)
+	// loop, id, remove,(index, index+1)
+
 	for index, course := range courseDB {
 		if course.CourseId == params["id"] {
 			courseDB = append(courseDB[:index], courseDB[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(params["id"] + " deleted")
+	json.NewEncoder(w).Encode(params["id"] + "Deleted..!")
 }
