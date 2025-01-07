4b0b25791f38a2f94b0bd50451b38ec788e50883
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-07 14:56:50 +0600

    7_Jan_25|2:56PM

--- myTaskAndProject/18Interface/main.go
+++ myTaskAndProject/18Interface/main.go
@@ -2,38 +2,33 @@ package main
 
 import "fmt"
 
+type paymentr interface {
+	pay(amount float32)
+}
+
 type payment struct {
-	gateWay strip
+	gateWay paymentr
 }
 
 func (p payment) makePayment(amount float32) {
 	// razarPaygw := strip{}
-	p.gateWay.pay(100)
-}
-
-type rezarpay struct{}
-
-func (r rezarpay) pay(amount float32) {
-	fmt.Println("making payment is using razarpay: ", amount)
-}
-
-type strip struct{}
-
-func (s strip) pay(amount float32) {
-	fmt.Println("making payment is using strip: ", amount)
+	p.gateWay.pay(amount)
 }
 
 type fakePayment struct{}
+
 func (f fakePayment) pay(amount float32) {
 	fmt.Println("making payment is usnig fake: ", amount)
 }
 
 func main() {
 	fmt.Println("Wellcome in go-interface")
-	stripPaymentGw := strip{}
+	// stripPaymentGw := strip{}
+	// razerPaymentGw := rezarpay{}
+	fakePayGw := fakePayment{}
 	newPayment := payment{
-		gateWay: stripPaymentGw,
+		gateWay: fakePayGw,
 	}
-	newPayment.makePayment(100)
+	newPayment.makePayment(200.3)	
 
 }
